fab6092c5ac70b70f60f89c15729ce9b012b4f22
irfanyazid-13032000 <irfanyazid18@gmail.com>
2023-08-10 10:14:14 +0700

    membuat table, model, controller

---
+++ database/migrations/2023_08_10_024636_cart.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->string('generated_card_number');
+            $table->integer('product_id');
+            $table->integer('price');
+            $table->integer('qty');
+            $table->integer('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
---
+++ database/migrations/2023_08_10_024909_receipt.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('receipts', function (Blueprint $table) {
+            $table->id();
+            // generated receipt_no from system
+            $table->string('receipt_no');
+            $table->integer('product_id');
+            $table->integer('price');
+            $table->integer('qty');
+            $table->integer('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
